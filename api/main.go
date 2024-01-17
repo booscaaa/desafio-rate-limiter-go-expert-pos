ad7ba67213df222a3196f3e1c139dacdabf487da
bosca <boscardinvinicius@gmail.com>
2024-01-17 13:53:34 -0300

    :recycle: refactor(main.go): remove viper init functions

--- api/main.go
+++ api/main.go
@@ -6,25 +6,16 @@ import (
 	"github.com/booscaaa/desafio-rate-limiter-go-expert-pos/ratelimiter"
 
 	"github.com/gin-gonic/gin"
-	"github.com/spf13/viper"
 )
 
-func init() {
-	viper.SetConfigFile(`config.json`)
-	err := viper.ReadInConfig()
-	if err != nil {
-		panic(err)
-	}
-}
-
 func main() {
-	limiter := ratelimiter.Initialize()
+	ratelimiter.Initialize()
 
 	router := gin.Default()
 	router.Use(func(c *gin.Context) {
 		ratelimiter.Middleware(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 			c.Next()
-		}), limiter.Storage).ServeHTTP(c.Writer, c.Request)
+		})).ServeHTTP(c.Writer, c.Request)
 	})
 
 	router.GET("/", func(c *gin.Context) {
